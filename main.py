af127ab2d4c663f12e132d5c1fe5eaeba18251d2
seeleng <seelengxd@gmail.com>
2020-12-17 19:27:54 +0800

    minor stuff

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ import PySimpleGUI as sg
 chromedriver_location = os.path.join(os.getcwd(), 'chromedriver')
 driver = webdriver.Chrome(chromedriver_location)
 
+
 def get_times(zc1, zc2):
     """given two zipcodes, go to google maps to parse the travelling time in (hour, minute) format"""
-    
-    #go to the google maps page and select driving
+
+    # go to the google maps page and select driving
     driver.get(f'https://www.google.com/maps/dir/Singapore+{zc1}/Singapore+{zc2}')
     time.sleep(0.1)
     driver.find_elements_by_css_selector("[aria-label='Driving']")[0].click()
 
-    #dont spam google + increasing delay to make the html load properly i think
+    # dont spam google + increasing delay to make the html load properly i think
     time.sleep(1.2)
 
-    #parse out times on the browser after taking the html
+    # parse out times on the browser after taking the html
     page = BeautifulSoup(driver.page_source, 'html.parser')
-    grabbed = page.find_all('div', {'class':'section-directions-trip-duration'})
-    parsed =  re.findall('(?:(\d+) hr)? ?(?:(\d+) min)', ' '.join(str(i) for i in grabbed))
+    grabbed = page.find_all('div', {'class': 'section-directions-trip-duration'})
+    parsed = re.findall('(?:(\d+) hr)? ?(?:(\d+) min)', ' '.join(str(i) for i in grabbed))
 
-    #get (hour:int, minute:int) format
+    # get (hour:int, minute:int) format
     processed = []
     for h, m in parsed:
         if not h:
@@ -36,31 +37,29 @@ def get_times(zc1, zc2):
     return processed
 
 
-
 def find_min_time(zipcodes):
     """return the 2 zipcode combination that has the shortest travelling time"""
-    
-    #if you need it to be both ways, just change it to permutations
+
+    # if you need it to be both ways, just change it to permutations
     combinations = itertools.combinations(zipcodes, 2)
     times = {}
     for c1, c2 in combinations:
         times[(c1, c2)] = min(get_times(c1, c2))
 
-    return min(times.items(), key=lambda i:i[1])
+    return min(times.items(), key=lambda i: i[1])
 
-    
 
 def main():
     """the gui that uses the above functions to find the shortest time"""
     layout = [
-        [sg.Text("Enter zipcodes here!")], 
-        [sg.Input(key='-IN-')], 
+        [sg.Text("Enter zipcodes here!")],
+        [sg.Input(key='-IN-')],
         [sg.Text('Zipcode 1: '), sg.Text('_______________', key="zc1")],
         [sg.Text('Zipcode 2: '), sg.Text('_______________', key="zc2")],
         [sg.Text('Hours: '), sg.Text('_______________', key="h")],
         [sg.Text('Minutes: '), sg.Text('_______________', key="m")],
         [sg.Button('Read'), sg.Exit()]
-        ]
+    ]
 
     # Create the window
     window = sg.Window("Zipcode stuff", layout)
@@ -69,11 +68,10 @@ def main():
         # Create an event loop
         while True:
             event, values = window.read()
-            # End program if user closes window or
-            # presses the OK button
             if event == 'Read':
                 zipcodes = re.split(' *, *', values['-IN-'])
                 (z1, z2), (hr, min_) = find_min_time(zipcodes)
+
                 window['zc1'].update(z1)
                 window['zc2'].update(z2)
                 window['h'].update(hr)
@@ -84,9 +82,10 @@ def main():
 
     except Exception as e:
         print(e)
-        
+
     finally:
         driver.close()
         window.close()
+
 
 main()
